b8987ca7bdc02e719465e21da7342dfe64dba802
Rafael Bolaños <animasdelmundo2@gmail.com>
2026-06-03 20:29:25 -0500

    refactor: inline test endpoints, remove TestController

--- src/HydraForge.Server/Program.cs
+++ src/HydraForge.Server/Program.cs
@@ -88,7 +88,37 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithTags("Test");
+
+app.MapGet("/throw", () =>
+{
+    throw new InvalidOperationException("Test exception");
+})
+.WithName("Throw")
+.WithTags("Test");
+
 app.Run();
 
 public partial class Program { }
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
 
